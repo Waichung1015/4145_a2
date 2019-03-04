629418ba84ed1768be5b86d5c665cb09c921773c
zixuannnn <48137765+zixuannnn@users.noreply.github.com>
2019-03-03 20:03:07 -0400

    Push MySQL DB - Zixuan Liu

--- Assi2_DB/assi2DB.sql
+++ Assi2_DB/assi2DB.sql
@@ -15,7 +15,8 @@ employer_name varchar(45),
 employer_title varchar(45),
 employer_dpt varchar(45),
 done bool,
-employee_name varchar(45)
+employee_name varchar(45),
+employee_id int
 );
 
 create table if not exists broker_mortgage_record(
@@ -27,15 +28,19 @@ user_company varchar(45),
 user_phone varchar(45),
 user_address varchar(45),
 user_mortgage decimal,
+employer_name varchar(45),
+employer_title varchar(45),
+employer_dpt varchar(45),
+emp_id int,
+user_salary int,
 have_submitted bool,
 have_emp_help bool,
-have_done bool,
 foreign key (user_id) references broker_userinfo(user_id),
 foreign key (form_id) references employer_form(form_id)
 );
 
 create table if not exists employee_info(
-emp_id int primary key,
+emp_id int auto_increment primary key,
 emp_name varchar(45),
 emp_title varchar(45),
 emp_work_time decimal,
@@ -45,9 +50,12 @@ emp_salary decimal
 create table if not exists employee_form(
 mortgage_id int,
 emp_id int,
-form_id int primary key,
+form_id int auto_increment primary key,
 address varchar(45),
-mortgage_money decimal,
-foreign key (mortgage_id) references broker_mortgage_record(mortgage_id),
+#foreign key (mortgage_id) references broker_mortgage_record(mortgage_id),
 foreign key (emp_id) references employee_info(emp_id)
 );
+
+insert into broker_userinfo (user_name, user_pwd, user_email) values ('name', 'pwd', 'name@email.com');
+insert into employee_info (emp_name, emp_title, emp_work_time, emp_salary) values ('Florence', 'employee', 40, 5486);
+insert into employee_info (emp_name, emp_title, emp_work_time, emp_salary) values ('Employer', 'employer', 40, 8000);
